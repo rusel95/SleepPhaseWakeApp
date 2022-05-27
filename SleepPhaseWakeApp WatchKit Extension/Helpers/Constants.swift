20e6c0f4bd6398452785e8b1c6da3094f5279c36
Ruslan Pppesku <ruslanpopesku95@gmail.com>
2022-05-27 16:37:09 +0300

    [add] Constants file

---
+++ SleepPhaseWakeApp WatchKit Extension/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  SleepPhaseWakeApp WatchKit Extension
+//
+//  Created by Ruslan Popesku on 27.05.2022.
+//
+
+import Foundation
+
+struct Constants {
+
+    static let defaultAnimationDuration: Double = 0.3
+    static let defaultProcessingDuration: TimeInterval = 30 * 60 // 30 minutes
+
+}
